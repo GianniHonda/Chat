093647f20c4b86ece888db6f538060b695fb535f
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-12-14 10:02:16 +0900

    Add files via upload

---
+++ Client/Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                IPEndPoint ipend = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+                socket.Connect(ipend);
+
+                Console.WriteLine("Conexão feita com sucesso !");
+
+                Console.Write("Digite sua mensagem: ");
+
+                String info = Console.ReadLine();
+
+                byte[] infoEnviar = Encoding.Default.GetBytes(info);
+
+                socket.Send(infoEnviar, infoEnviar.Length, SocketFlags.None);
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine("The isn´t found !");
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+            socket.Close();
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint iPEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+            socket.Bind(iPEnd);
+
+            socket.Listen(5);
+
+            Console.Write("Escutando...");
+
+            Socket escutar = socket.Accept();
+
+            byte[] bytes = new byte[255];
+
+            int tamanho = escutar.Receive(bytes,0,bytes.Length, SocketFlags.None);
+
+            Array.Resize(ref bytes, tamanho);
+
+            Console.Write("Cliente falou: " + Encoding.Default.GetString(bytes));
+
+            escutar.Close();
+
+            Console.WriteLine("Servidor fechado");
+        }
+    }
+}
